a3c8ac1cde1f1d1f58981f5fcc11cba3df860ab3
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-06 16:02:16 +0100

    added features

--- lib/card/quoteCard.dart
+++ lib/card/quoteCard.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:genesis/quote.dart';
+import 'package:genesis/card/quote.dart';
 import 'quote.dart';
 
 
